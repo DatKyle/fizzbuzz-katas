4a5563c7da9ed29e7c89c04295be44ea210e036b
Kyle burns <datasskyle@gmail.com>
2024-10-14 23:13:20 +0100

    refactor: change values to constants

--- C#/fizzbuzz-reflection/FizzBuzz.Tests/FizzBuzzTests.cs
+++ C#/fizzbuzz-reflection/FizzBuzz.Tests/FizzBuzzTests.cs
@@ -5,12 +5,17 @@ namespace FizzBuzz.Tests
 {
     public class FizzBuzzTests
     {
+        private const int Zero = 0;
+        private const int Number = 1;
+        private const int Fizz = 3;
+        private const int Buzz = 5;
+        private const int FizzBuzz = 15;
 
         [Fact]
         public void Zero_ShouldBe0()
         {
             FizzBuzzFactory fizzBuzzFactory = new();
-            IFizzBuzzAction? result = fizzBuzzFactory.Process(0);
+            IFizzBuzzAction? result = fizzBuzzFactory.Process(Zero);
             Assert.NotNull(result);
             Assert.Equal(typeof(ZeroAction), result.GetType());
             Assert.Equal("0", result.Process());
@@ -20,7 +25,7 @@ namespace FizzBuzz.Tests
         public void One_ShouldBe1()
         {
             FizzBuzzFactory fizzBuzzFactory = new();
-            IFizzBuzzAction? result = fizzBuzzFactory.Process(1);
+            IFizzBuzzAction? result = fizzBuzzFactory.Process(Number);
             Assert.Null(result);
         }
 
@@ -28,7 +33,7 @@ namespace FizzBuzz.Tests
         public void Three_ShouldBeFizz()
         {
             FizzBuzzFactory fizzBuzzFactory = new();
-            IFizzBuzzAction? result = fizzBuzzFactory.Process(3);
+            IFizzBuzzAction? result = fizzBuzzFactory.Process(Fizz);
             Assert.NotNull(result);
             Assert.Equal(typeof(FizzAction), result.GetType());
             Assert.Equal("fizz", result.Process());
@@ -38,7 +43,7 @@ namespace FizzBuzz.Tests
         public void Five_ShouldBeBuzz()
         {
             FizzBuzzFactory fizzBuzzFactory = new();
-            IFizzBuzzAction? result = fizzBuzzFactory.Process(5);
+            IFizzBuzzAction? result = fizzBuzzFactory.Process(Buzz);
             Assert.NotNull(result);
             Assert.Equal(typeof(BuzzAction), result.GetType());
             Assert.Equal("buzz", result.Process());
@@ -48,7 +53,7 @@ namespace FizzBuzz.Tests
         public void MultipleOf_3_and_5_ShouldBeFizzBuzz()
         {
             FizzBuzzFactory fizzBuzzFactory = new();
-            IFizzBuzzAction? result = fizzBuzzFactory.Process(15);
+            IFizzBuzzAction? result = fizzBuzzFactory.Process(FizzBuzz);
             Assert.NotNull(result);
             Assert.Equal(typeof(FizzBuzzAction), result.GetType());
             Assert.Equal("fizzbuzz", result.Process());
